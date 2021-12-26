56c0044620b02f714f67a30bccf9db62e28120e9
sophnagle <sophnagle@gmail.com>
2021-12-26 16:34:40 +0000

    Add basic set up for three.js project:

--- planet.js
+++ planet.js
@@ -13,3 +13,18 @@ const scene = new THREE.Scene()
 
 const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window/innerHeight, 0.1, 10000)
 camera.position.z = -3000
+
+const animate = function () {
+    renderer.render(scene, camera)
+
+    requestAnimationFrame(animate)
+}
+animate()
+
+// if window is resized
+window.addEventListener("resize", function () {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera/updateProjectionMatrix()
+
+    renderer.setSize(window.innerWidth, windwo.innerHeight)
+})
